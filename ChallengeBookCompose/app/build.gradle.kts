addfa178b0cd0bc983171e1be77423d1c1ed1fa6
Valen <rodriguezvalentin1205@gmail.com>
2025-08-13 09:27:16 -0300

    pokeapi dependencias

--- ChallengeBookCompose/app/build.gradle.kts
+++ ChallengeBookCompose/app/build.gradle.kts
@@ -41,6 +41,9 @@ android {
 
 dependencies {
 
+
+    implementation("com.squareup.retrofit2:retrofit:3.0.0")
+    implementation ("com.squareup.retrofit2:converter-gson:3.0.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
